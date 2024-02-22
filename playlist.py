3fcbfc39505d154bf98b36b6051c8f04504ba23a
itsKia2 <omerkarimi29@gmail.com>
2024-02-22 18:44:01 -0500

    fixed mySavedTracks(); saves to ./data/likedSongs.csv now

--- playlist.py
+++ playlist.py
@@ -21,13 +21,25 @@ playlist_features_list = [
 
 
 def mySavedTracks(sp):
-    response = sp.current_user_saved_tracks(limit=50)
-    counter = 0
-    for item in response["items"]:
-        track = item["track"]
-        print("%d - %32.32s %s" % (counter, track["artists"][0]["name"], track["name"]))
-        counter = counter + 1
+    playlist_df = pd.DataFrame(columns=playlist_features_list)
+    response = sp.current_user_saved_tracks(limit=50)["items"]
+    for track in response:
+        playlist_features = {}  # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["track_name"] = track["track"]["name"]
+        playlist_features["track_id"] = track["track"]["id"]
 
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[4:]:
+            playlist_features[feature] = audio_features[feature]
+
+        # Concat the dfs
+        track_df = pd.DataFrame(playlist_features, index=[0])
+        playlist_df = pd.concat([playlist_df, track_df], ignore_index=True)
+
+    playlist_df.to_csv("./data/likedSongs.csv")
 
 def tracksFromPlaylist(sp, creator, pID):
     # Create empty dataframe
@@ -52,8 +64,7 @@ def tracksFromPlaylist(sp, creator, pID):
         track_df = pd.DataFrame(playlist_features, index=[0])
         playlist_df = pd.concat([playlist_df, track_df], ignore_index=True)
 
-    print("success: ")
-    print(playlist_df)
+    playlist_df.to_csv("./data/playlist.csv")
     return playlist_df
 
 
