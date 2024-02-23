bf5ec27e8642c7607b7b08bf0808acdc15a289a5
itsKia2 <omerkarimi29@gmail.com>
2024-02-23 03:04:24 -0500

    minor changes to requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from client import *
 from playlist import *
 from ari import *
+from recommender import *
 
 creator = "itsKia2"
 url = getPlaylistID(
@@ -10,7 +11,12 @@ url = getPlaylistID(
 # pid = getPlaylistID(url)
 
 sp = connectClient(creator)
+
+
+# FIRST CHECK IF CSV FILES HAVE ALREADY BEEN CREATED BEFORE QUERYING SPOTIFY <----------------------------------------------
 # mySavedTracks(sp)
 # tracksFromPlaylist(sp, creator, url)
 # mySavedTracks(sp)
-extractFeat(sp, "./data/likedSongs.csv")
+
+extractFeat(sp, "./data/SHORTdataset.csv")
+processAllSongs()
