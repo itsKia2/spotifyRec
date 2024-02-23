653feaf873e473a7d8f98294af7bba39f18af668
itsKia2 <omerkarimi29@gmail.com>
2024-02-23 00:18:40 -0500

    added ./data/

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ creator = "itsKia2"
 url = getPlaylistID(
     "https://open.spotify.com/playlist/1IjnMCB8vrBAgVl3BWaiQ7?si=dfcab64aad714bbc"
 )
+
+# pid = getPlaylistID(url)
+
 sp = connectClient(creator)
+# mySavedTracks(sp)
+# tracksFromPlaylist(sp, creator, url)
 # mySavedTracks(sp)
 extractFeat(sp, "./data/likedSongs.csv")
